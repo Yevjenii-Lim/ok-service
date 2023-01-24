3a6b9c0cc6f7aeb9f9a09cff280556933c4457f6
Yevjenii-Lim <pacmanlim27@gmail.com>
2023-01-24 14:19:25 +0200

    added div to view

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -5,22 +5,58 @@ console.log('copart extention');
 // const body = document.body('.row .no-margin');
 
 const App: React.FC<{}> = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      {' '}
+      <div className="lot-details-page panel panel-heading">
+        <h3>OK Service</h3>
+      </div>
+      <div className="panel-content"></div>
+    </>
+  );
 };
 
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
 const rootElement = document.createElement('div');
-// const row = document.querySelector('.row.no-margin');
-const bidInfo = document.getElementById('bid-information-id');
-// const ads = document.getElementById('copart-r-view-2');
-// ads.style.display = 'none';
-const body = document.body;
+
 rootElement.setAttribute('id', 'extensionRoot');
-bidInfo.appendChild(rootElement);
+rootElement.classList.add('panel');
+rootElement.classList.add('clr');
 
-const reactRoot = ReactDOM.createRoot(document.getElementById('extensionRoot'));
+waitForElm(
+  '#bid-information-id > div.print-lot-banner.clearfix.desktop-only.borderNone'
+).then((elm: HTMLElement) => {
+  elm.style.display = 'none';
+  waitForElm('#bid-information-id').then((elm: HTMLElement) => {
+    console.log('Element is ready');
+    elm.appendChild(rootElement);
+    const reactRoot = ReactDOM.createRoot(
+      document.getElementById('extensionRoot')
+    );
 
-reactRoot.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    reactRoot.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
+});
