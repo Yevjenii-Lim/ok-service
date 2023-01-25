746ae1c22d364b84f78ca1be424bf4de8d8fe313
Yevjenii-Lim <pacmanlim27@gmail.com>
2023-01-25 20:22:05 +0200

    working on getting data from page to add on our display

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -3,15 +3,40 @@ import React, { useEffect, useState } from 'react';
 
 console.log('copart extention');
 // const body = document.body('.row .no-margin');
+// interface props {
+//   test: string;
+// }
 
-const App: React.FC<{}> = () => {
+type props = {
+  test?: string;
+};
+
+const App: React.FC<props> = ({ test }: props) => {
   return (
     <>
       {' '}
       <div className="lot-details-page panel panel-heading">
         <h3>OK Service</h3>
       </div>
-      <div className="panel-content"></div>
+      <div className="panel-content">
+        <div className="clearfix">
+          <label
+            htmlFor="location"
+            data-name="lotdetailSaleinformationlocationlabel"
+          >
+            Delivery from:
+          </label>
+          <span>
+            <a
+              href="google.com"
+              target="_blank"
+              data-name="lotdetailSaleinformationlocationvalue"
+            >
+              {test}
+            </a>
+          </span>
+        </div>
+      </div>
     </>
   );
 };
@@ -54,11 +79,15 @@ waitForElm(
     elm.appendChild(rootElement);
     reactRoot = ReactDOM.createRoot(document.getElementById('extensionRoot'));
 
-    reactRoot.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
+    waitForElm(
+      '#lot-details > div > div.container-fluid.lot-details-description > div > div > div:nth-child(1) > div.col-lg-9.col-md-8.col-xs-12.image-location.p-0-i > div.d-flex > div.d-flex.flex-dir-col.lot-detail-section > div.p-0.lot-information.d-flex.f-g1 > div > div.tab-content.f-g1.d-f > div.panel-content.clearfix.f-g1.d-flex-column.full-width > div > div:nth-child(24) > div > span'
+    ).then((link: HTMLElement) => {
+      reactRoot.render(
+        <React.StrictMode>
+          <App test={link.textContent} />
+        </React.StrictMode>
+      );
+    });
   });
 });
 
